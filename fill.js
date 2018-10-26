bb3773e4f973ef5faf1cd4a0b0bc08cf067ca474
Emil Segerbäck <emilsegerback@gmail.com>
2018-10-26 18:43:23 +0200

    Add element hiding

--- fill.js
+++ fill.js
@@ -1,1 +1,43 @@
-console.log("in fill")
+// TODO: cancel on escape
+// TODO: remove event handler on cancel
+
+var hoverElement = null;
+var oldBackground = null;
+
+function resetHover() {
+    if (hoverElement != null && oldBackground != null) {
+        hoverElement.style.backgroundColor = oldBackground;
+    }
+    hoverElement = null;
+    oldBackground = null;
+}
+
+window.onmouseover = function(e) {
+    if (hoverElement != null) {
+        resetHover()
+    }
+
+    hoverElement = e.target;
+    oldBackground = hoverElement.style.backgroundColor;
+    hoverElement.style.backgroundColor = "red";
+};
+
+function hideOthers(clicked, searchNode) {
+    for (let node of searchNode.children) {
+        if (node !== clicked) {
+            node.style.display = "none";
+            hideOthers(clicked, node);
+        }
+    }
+}
+
+window.onclick = function(e) {
+    resetHover();
+
+    clicked = e.target;
+    clicked.style.margin = 0;
+    clicked.style.height = "100%";
+    clicked.style.width = "100%";
+
+    hideOthers(clicked, document.body);
+}
